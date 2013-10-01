eb42b056f4762aaa4de59b038d6c59ae288c1d99
guy.paddock <none@none>
2013-10-01 04:41:39 -0400

    Added equals() and hashCode() to DateRange and Entity.Reference, so that before-and-after exclusion works properly.

--- src/com/redbottledesign/drupal/Entity.java
+++ src/com/redbottledesign/drupal/Entity.java
@@ -131,12 +131,73 @@ public abstract class Entity<T extends Entity<?>>
     }
 
     @Override
+    public int hashCode()
+    {
+      final int prime = 31;
+            int result = 1;
+
+      result = prime * result + ((entityType == null) ? 0 : entityType.hashCode());
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+      if (this == obj)
+        return true;
+
+      if (obj == null)
+        return false;
+
+      if (getClass() != obj.getClass())
+        return false;
+
+      Reference<?> other = (Reference<?>)obj;
+
+      if (entityType == null)
+      {
+        if (other.entityType != null)
+          return false;
+      }
+
+      else if (!entityType.equals(other.entityType))
+        return false;
+
+      if (id == null)
+      {
+        if (other.id != null)
+          return false;
+      }
+
+      else if (!id.equals(other.id))
+        return false;
+
+      if (uri == null)
+      {
+        if (other.uri != null)
+          return false;
+      }
+
+      else if (!uri.equals(other.uri))
+        return false;
+
+      return true;
+    }
+
+    @Override
     public String toString()
     {
-      return this.getClass().getSimpleName()  + " [" +
-             "id="          + this.id         + ", " +
-             "entityType="  + this.entityType + ", " +
-             "uri="         + this.uri        +
+      Class<?>  thisClass = this.getClass();
+      String    enclosingClassName = thisClass.getEnclosingClass().getSimpleName(),
+                thisClassName      = thisClass.getSimpleName();
+
+      return enclosingClassName + "$" + thisClassName + " [" +
+             "id="              + this.id             + ", " +
+             "entityType="      + this.entityType     + ", " +
+             "uri="             + this.uri            +
              "]";
     }
   }
